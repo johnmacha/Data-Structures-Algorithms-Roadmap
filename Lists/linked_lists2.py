a601d7324ef043e8144079d2b1a9eb8287e611e5
johnmacha <jontehmacha@gmail.com>
2025-11-27 22:53:08 +0300

    chore: update 1 for linked_list2

--- Lists/linked_lists2.py
+++ Lists/linked_lists2.py
@@ -35,9 +35,45 @@ class LinkedList:
 
         itr.next = Node(data, None)
 
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+
+        return count 
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid index")
+        
+        if index==0:
+            self.head = self.head.next
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_at_beginnning(62)
     ll.insert_at_beginnning(17)
     ll.insert_at_end(99)
+    ll.insert_values(["banana","mango","grapes","orange"])
+    print("length:", ll.get_length())
+    ll.print()
+    ll.remove_at(1)
+    ll.remove_at(-1)
     ll.print()
