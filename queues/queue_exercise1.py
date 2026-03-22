b2bd6e1e891ff4cc6f5fa1da82b358d0b75f86bb
johnmacha <jontehmacha@gmail.com>
2026-03-22 13:46:39 +0300

    fix: fixed the place_order function issue

--- queues/queue_exercise1.py
+++ queues/queue_exercise1.py
@@ -19,32 +19,34 @@ class Queue():
     def size(self):
         return len(self.order)
 
-orders = ['pizza','samosa','pasta','biryani','burger']
-q = Queue()
-def place_order(p):
-    for o in p:
-        q.enqueue(o)
-        print('Placing order :',place_order(o))
-        time.sleep(0.5)
+if __name__ == '__main__':
+    orders = ['pizza','samosa','pasta','biryani','burger']
+    q = Queue()
+    def place_order(p):
+        for o in p:
+            q.enqueue(o)
+            print('Placing order :', o)
+            # print(type(p), p)
+            time.sleep(0.5)
 
 
-def serve_order():
-    time.sleep(1) #Also start this thread 1 second after place order thread is started.
-    for i in range(len(orders)):
-        order = q.dequeue()
-        print("Now serving: ",order)
-        time.sleep(2)
-        # print(serve_order())
+    def serve_order():
+        time.sleep(1) #Also start this thread 1 second after place order thread is started.
+        for i in range(len(orders)):
+            order = q.dequeue()
+            print("Now serving: ",order)
+            time.sleep(2)
+            # print(serve_order())
 
-t1 = threading.Thread(target = place_order,args=(orders,))
-t2 = threading.Thread(target = serve_order)
+    t1 = threading.Thread(target = place_order,args=(orders,))
+    t2 = threading.Thread(target = serve_order)
 
-t = time.time()
-t1.start()
-time.sleep(1)
-t2.start()
+    t = time.time()
+    t1.start()
+    time.sleep(1)
+    t2.start()
 
-t1.join()
-t2.join()
+    t1.join()
+    t2.join()
 
-print('process done in :',time.time()-t)
+    print('process done in :',time.time()-t)
